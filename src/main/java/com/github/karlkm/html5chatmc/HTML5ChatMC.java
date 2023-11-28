7ced230a76d4897410b963c6842bae9f74197bfb
karl-k-m <karl.k.muldia@gmail.com>
2023-11-28 13:03:10 +0200

    Initial commit.

---
+++ src/main/java/com/github/karlkm/html5chatmc/HTML5ChatMC.java
@@ -0,0 +1,17 @@
+package com.github.karlkm.html5chatmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HTML5ChatMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
